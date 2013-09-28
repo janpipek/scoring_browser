454267c1acf5f21139c19bae0d08462bf58cd0bc
Jan Pipek <jan.pipek@gmail.com>
2013-09-28 23:05:08 +0200

    Make the source module-like

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#
+# scoring_browser --- Simple Qt application for browsing scoring outputs in Geant4
+#
+# Copyright (C) 2012 Jan Pipek (jan.pipek@gmail.com)
+#
+# This file may be distributed without limitation.
+#
+from data_matrix import DataMatrix
+from qt4_ui import ApplicationWindow
